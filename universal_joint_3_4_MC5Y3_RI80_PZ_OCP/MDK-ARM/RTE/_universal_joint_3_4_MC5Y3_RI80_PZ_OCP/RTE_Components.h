e3393fb60c95e87fecd29b15b724d4d44695da5a
Łukasz Łęcki <lukasz.lecki@pomagier.info>
2022-07-04 15:26:38 +0200

    update PZ to EEPROM

---
+++ universal_joint_3_4_MC5Y3_RI80_PZ_OCP/MDK-ARM/RTE/_universal_joint_3_4_MC5Y3_RI80_PZ_OCP/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'universal_joint_3_4_MC5Y3_RI80_PZ_OCP' 
+ * Target:  'universal_joint_3_4_MC5Y3_RI80_PZ_OCP' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32g4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
